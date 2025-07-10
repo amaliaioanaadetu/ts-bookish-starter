987c4fcdb487fc5b9dd06b460dcceccd80308f40
amaliaioanaadetu <Amalia.Adetu@softwire.com>
2025-07-10 18:17:46 +0300

    added post request but only works to retrieve things from the body

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,15 +1,42 @@
-create table library (
-	title VARCHAR(50) NOT NULL,
-	author VARCHAR(50) NOT NULL,
-	ISBN VARCHAR(30) NOT NULL,
-	copies VARCHAR(3),
-	copies_available VARCHAR(3)
+create table library(
+                        id INT PRIMARY KEY,
+                        title VARCHAR(50) NOT NULL,
+                        author VARCHAR(50) NOT NULL,
+                        ISBN VARCHAR(30) NOT NULL,
+                        copies INT NOT NULL,
+                        COPIES_AVAILABLE varchar(3) NOT NULL
 )
 
-ALTER TABLE dbo.library
-ALTER COLUMN ISBN VARCHAR(30) NOT NULL;
+create table authors(
+                        id INT PRIMARY KEY,
+                        name VARCHAR(50) NOT NULL
+)
 
-INSERT INTO dbo.library
-VALUES ('Baltagul', 'Mihail Sadoveanu', '978-606-93355-0-5', 5, 3);
 
-select * from dbo.library
+create table book_authors(
+                             book_id INT,
+                             author_id INT,
+                             FOREIGN KEY (book_id) REFERENCES library(id),
+                             FOREIGN KEY (author_id) REFERENCES authors(id)
+)
+
+create table users(
+                      id INT PRIMARY KEY,
+                      name VARCHAR(50) NOT NULL,
+                      email VARCHAR(50) NOT NULL,
+                      password_hash VARCHAR(50) NOT NULL,
+)
+
+create table loans(
+                      id INT PRIMARY KEY,
+                      user_id INT,
+                      book_id INT,
+                      loan_date DATETIME,
+                      due_date DATETIME
+)
+
+    insert into dbo.library
+values(1, 'Enigma Otilie', 'George Calinescu', '978-973-104-238-1', 8, 6);
+
+insert into dbo.library
+values(2, 'Iona', 'Marin Sorescu', '	973-98642-5-2', 7, 1)
--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,22 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { Connection, ConnectionConfiguration, Request as TediousRequest } from 'tedious';
-import {Book} from './Book'
+import { Book } from '../models/Book';
+import { configuration } from '../configuration';
 
-const config: ConnectionConfiguration = {
-    server: 'localhost',
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'amaadeioa',
-            password: 'amaliaioana12!',
-        },
-    },
-    options: {
-        database: 'bookish',
-        trustServerCertificate: true,
-        encrypt: false,
-    },
-};
 
 class BookController {
     router: Router;
@@ -31,7 +17,7 @@ class BookController {
 
     getBooks(req: Request, res: Response) {
         try {
-            const connection = new Connection(config);
+            const connection = new Connection(configuration);
             const books: Book[] = [];
 
             connection.on('connect', (err) => {
@@ -78,7 +64,7 @@ class BookController {
 
     getBook(req: Request, res: Response) {
         try {
-            const connection = new Connection(config);
+            const connection = new Connection(configuration);
             const books: Book[] = [];
 
             connection.on('connect', (err) => {
@@ -126,18 +112,92 @@ class BookController {
                 connection.execSql(request);
             });
             connection.connect();
-
         } catch (error) {
             res.status(500).send('Server Error');
         }
     }
 
     createBook(req: Request, res: Response) {
-        // TODO: implement functionality
-        return res.status(500).json({
-            error: 'server_error',
-            error_description: 'Endpoint not implemented yet.',
-        });
+        const connection = new Connection(configuration);
+        const requestData = req.body;
+        const id = parseInt(requestData['id']);
+        if (isNaN(id)) {
+            return res.status(400).json({
+                error: 'bad request',
+                error_description: 'Not a number.',
+            });
+        }
+
+        const title = requestData['title'];
+        if (title === undefined || title === null || title === '') {
+            return res.status(400).json({
+                error: 'bad request',
+                error_description: 'Not a title.',
+            });
+        }
+
+        const author = requestData['author'];
+        if (author === undefined || author === null || author === '') {
+            return res.status(400).json({
+                error: 'bad request',
+                error_description: 'Not an author.',
+            });
+        }
+
+        const isbn = requestData['isbn'];
+        if (isbn === undefined || isbn === null || isbn === '') {
+            return res.status(400).json({
+                error: 'bad request',
+                error_description: 'Not an isbn.',
+            });
+        }
+
+        const copies = parseInt(requestData['copies']);
+        if (isNaN(copies)) {
+            return res.status(400).json({
+                error: 'bad request',
+                error_description: 'Not a number.',
+            });
+        }
+
+        const copiesAvailable = parseInt(requestData['available copies']);
+        if (isNaN(copiesAvailable)) {
+            return res.status(400).json({
+                error: 'bad request',
+                error_description: 'Not a number.',
+            });
+        }
+
+        try {
+            connection.on('connect', (err) => {
+                if (err) {
+                    console.error('Connection error:', err);
+                    res.status(500).send('Failed to connect to database');
+                    return;
+                }
+
+                const sql = `insert into dbo.library\nvalues(${id}, ${title}, ${author}, ${isbn}, ${copies}, ${copiesAvailable});`;
+                const request = new TediousRequest(sql, (err) => {
+                    if (err) {
+                        console.error('Request error:', err);
+                        res.status(500).send('Query failed' + err.message);
+                        connection.close();
+                    } else {
+                        res.status(200).send('Book added');
+                        connection.close();
+                    }
+                });
+                connection.execSql(request);
+            });
+            connection.connect();
+        } catch (error) {
+            res.status(500).send('Server Error');
+        }
+
+        // return res.status(500).json({
+        //     error: 'server_error',
+        //     error_description: 'Endpoint not implemented yet.',
+        // });
     }
 }
 
